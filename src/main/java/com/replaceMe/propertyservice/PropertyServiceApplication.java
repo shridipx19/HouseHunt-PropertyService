7bad0e6407d9df1276689ddde8f589750fb5c166
TeyjusDarshan <teyjsdarshan@gmail.com>
2025-01-19 16:30:26 +0530

    added auth to property service

--- src/main/java/com/replaceMe/propertyservice/PropertyServiceApplication.java
+++ src/main/java/com/replaceMe/propertyservice/PropertyServiceApplication.java
@@ -3,7 +3,7 @@ package com.replaceMe.propertyservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.RepMe.Auth", "com.replaceMe.propertyservice"})
 public class PropertyServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PropertyServiceApplication.class, args);
